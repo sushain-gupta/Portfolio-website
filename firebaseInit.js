3733cb15168b0657f3c04692e8f756e48a92698d
SushainGupta <sushaingupta135@gmail.com>
2023-03-10 13:12:03 +0530

    Update: minor chnages

--- firebaseInit.js
+++ firebaseInit.js
@@ -5,7 +5,7 @@
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://sushains-portfolio-default-rtdb.firebaseio.com/",
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
 };
 
 firebase.initializeApp(firebaseConfig);
