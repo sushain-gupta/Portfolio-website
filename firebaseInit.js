6ec78cf03627ec953552bafdfa18cf26b31e5df7
SushainGupta <sushaingupta135@gmail.com>
2023-03-11 00:58:33 +0530

    Fix: Minor fixes and added toasts for dev mode

--- firebaseInit.js
+++ firebaseInit.js
@@ -5,7 +5,7 @@
 // TODO: Replace the following with your app's Firebase project configuration.
 const firebaseConfig = {
   // The value of `databaseURL` depends on the location of the database.
-  databaseURL: "https://public-portfolio-27920-default-rtdb.firebaseio.com/",
+  databaseURL: "https://public-portfolio-27920-default-rtdb.firebaseio.com",
 };
 
 firebase.initializeApp(firebaseConfig);
